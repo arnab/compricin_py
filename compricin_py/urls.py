10cc4f9a30179ebdcae7d8d31731c01a446037ca
Arnab Deka <arnab.deka@gmail.com>
2013-07-15 17:17:58 +0530

    switch the main URL from /prices to /search

--- compricin_py/urls.py
+++ compricin_py/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^prices/', include('aggregator.urls', namespace='aggregator')),
+    url(r'^search/', include('aggregator.urls', namespace='aggregator')),
     # url(r'^$', 'compricin_py.views.home', name='home'),
     # url(r'^compricin_py/', include('compricin_py.foo.urls')),
 
